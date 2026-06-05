68baceaf79df1f0892fa5dbd2080e44bfc485386
Yume05-dev <yume@users.noreply.github.com>
2026-06-05 16:13:45 -0400

    ups

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -1,5 +1,5 @@
 ﻿plugins {
-z    id("com.android.application")
+    id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
